7302e51d008bbae297a3cfc464ac8114091b1e31
Jonathan Luk <jluk87@gmail.com>
2019-08-16 19:43:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // your code here
 function getRepositories() {
   const req = new XMLHttpRequest;
-  
+  req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos')
   req.send();
+}
+
+function displayRepositories() {
+
+  
 }
