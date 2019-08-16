2ebf0353e3f661288cf4ba75f7cb4923203fac48
Jonathan Luk <jluk87@gmail.com>
2019-08-16 20:53:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -18,5 +18,6 @@ function displayRepositories() {
 }
 
 function getCommits(repo) {
-
+  const req = new XMLHttpRequest
+  req.open('GET', `https://api.github.com/repos/${repo.login}/${repo.name}`)
 }
