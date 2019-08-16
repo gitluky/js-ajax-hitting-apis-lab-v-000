e9e65ecc581532809aa8e0322603d636dfdb59c1
Jonathan Luk <jluk87@gmail.com>
2019-08-16 22:17:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ function displayRepositories() {
   const repos = `<ul>${ reposResponse
     .map(
       repo =>
-      '<li>' + repo.name + ' - <a href="' + repo.html_url + '">' + repo.html_url + '</a> + ' + '- <a href="#" data-repo="' + repo.name + '" onclick="' + getCommits(repo) + '">Get Commits</a></li>'
+      '<li>' + repo.name + ' - <a href="' + repo.html_url + '">' + repo.html_url + '</a> + ' + '- <a href="#" data-repo="' + repo.name + '" onclick="' + getCommits(this) + '">Get Commits</a></li>'
     ).join(' ')}</ul>`;
   document.getElementById('repositories').innerHTML = repos;
 }
