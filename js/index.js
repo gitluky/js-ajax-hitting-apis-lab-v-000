a4e094e39c134baa4ab6c1a4a4f67a870d95be05
Jonathan Luk <jluk87@gmail.com>
2019-08-16 22:37:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ function displayRepositories() {
 
 function getCommits(repo) {
   console.log(repo)
-  const fullName = repo.dataset.repo;
+  const fullName = repo.full_name;
   const req = new XMLHttpRequest;
   req.addEventListener('load', displayCommits);
   req.open('GET', `https://api.github.com/repos/${fullName}/commits`);
